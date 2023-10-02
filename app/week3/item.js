5f5fcbfe0ee07810bbcf989fc68e02632be95a7e
TysonHEdu <tyson.harder@edu.sait.ca>
2023-10-02 15:21:27 -0600

    Tailwind added

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = ({name,quantity,category} ) => {
     return (
-        <li>
+        <li className="border p-4 w-1/3 m-3">
             <strong>Name:</strong> {name}, <strong>Quantity:</strong> {quantity}, <strong>Category:</strong> {category}
         </li>
     )
